d8d6fcd57184e67be007ec97667d4fbe92878ccd
Carlos Alain Gamarra <carlosalaingamarra@gmail.com>
2022-12-28 19:57:18 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import datetime
+from django.db.models.fields.files import ImageField
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = ImageField(upload_to='blog/images/')
+    date = models.DateField(datetime.date.today)
